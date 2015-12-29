765a2e9b7eac14e7e3d589f579c830d9276d3096
Jonathan Garbee <jonathan@garbee.me>
2015-12-29 14:12:40 -0500

    Add babel config for helping setup the output file.

--- gulp/config.babel.js
+++ gulp/config.babel.js
@@ -24,5 +24,11 @@ export const AUTOPREFIXER_BROWSERS = [
 export const SOURCES = [
   'src/drawer/drawer.babel.js',
   'src/snackbar/snackbar.babel.js',
-  'src/textfield/textfield.babel.js'
+  'src/textfield/textfield.babel.js',
+  'src/icon-toggle/icon-toggle.babel.js'
 ];
+
+export const BABEL = {
+  createClassFunction: 'var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();',
+  classCallCheck: 'function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }'
+};
