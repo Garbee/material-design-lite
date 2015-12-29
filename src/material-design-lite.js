c5f5e088a405de40aabf9382400a1d9dbf52b38d
Jonathan Garbee <jonathan@garbee.me>
2015-12-29 14:11:51 -0500

    Remove main MDL js file.

--- src/material-design-lite.js
+++
@@ -1,4 +0,0 @@
-import Drawer from 'drawer/drawer.babel';
-import Snackbar from 'snackbar/snackbar.babel';
-import Textfield from 'textfield/textfield.babel';
-import IconToggle from 'icon-toggle/icon-toggle.babel';
